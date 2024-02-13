fab6d7174bc26428797e38656cd7c66438bfa21f
Lyuboslav Mihalchev <117465382+lyuboslavm@users.noreply.github.com>
2024-02-13 13:13:12 +0200

    PushTest

--- ForumApp/ForumApp/Data/DataConstants.cs
+++ ForumApp/ForumApp/Data/DataConstants.cs
@@ -6,6 +6,9 @@
         {
             public const int TitleMaxLenght = 50;
             public const int ContentMaxLenght = 1500;
+
+            public int Test = 1890;
+            public string Trst2 = "Test";
         }
     }
 }
